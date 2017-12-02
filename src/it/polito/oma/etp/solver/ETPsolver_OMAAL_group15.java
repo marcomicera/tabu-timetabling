5ea97d1e850bc4b8d1dc5a6a030fc23038beb723
Marco Micera <marco.micera@gmail.com>
2017-12-02 17:29:02 +0100

    "Stream not marked" I/O exception fix; new instances uploaded

--- src/it/polito/oma/etp/solver/ETPsolver_OMAAL_group15.java
+++ src/it/polito/oma/etp/solver/ETPsolver_OMAAL_group15.java
@@ -7,6 +7,12 @@ public class ETPsolver_OMAAL_group15 {
 	public static String instanceName;
 	
 	public static void main(String[] args) {
+		if(args.length != 1) {
+			System.err.println(
+				"Usage: java it.polito.oma.etp.solver.ETPsolver_OMAAL_group15 instance_number"
+			);
+			System.exit(1);
+		}
 		
 		instanceName = args[0];
 		InputReader.getBenchmark("res\\" + instanceName);
